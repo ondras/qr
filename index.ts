4071c12ddec2af411181f3ca6f5c52be1f76d187
Ondřej Žára <ondrej.zara@gmail.com>
2021-11-08 17:15:18 +0100

    cleanup

--- index.ts
+++ index.ts
@@ -1,22 +1,21 @@
 import { listenAndServe } from "https://deno.land/std/http/server.ts";
 
+
+const headers = { "Content-type": "image/jpeg" };
+
 async function handler(req: Request) {
-  let referer = req.headers.get("referer");
-  if (referer) {
-    let url = new URL("https://api.qrserver.com/v1/create-qr-code/");
-    let sp = url.searchParams;
-    sp.set("data", referer);
-    sp.set("format", "png");
-    sp.set("qzone", "1");
-    return fetch(url);
-  } else {
-    try {
-      const img = await Deno.readFile("hypnotoad.jpg");
-      return new Response(img);
-    } catch (e) {
-      return new Response(e.message);
-    }
-  }
+	let referer = req.headers.get("referer");
+	if (!referer) {
+		const img = await Deno.readFile("hypnotoad.jpg");
+		return new Response(img, {headers});
+	}
+
+	let url = new URL("https://api.qrserver.com/v1/create-qr-code/");
+	let sp = url.searchParams;
+	sp.set("data", referer);
+	sp.set("format", "png");
+	sp.set("qzone", "1");
+	return fetch(url.toString());
 }
 
 await listenAndServe(":8000", handler);
